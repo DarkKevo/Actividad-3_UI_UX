0591c4ee275e1b8fe3ccd8f264227ce882692b5c
DarkKevo <kevinaraujogonzalez@gmail.com>
2022-03-09 15:39:32 -0400

    Carrusel de los Servicios Completado y Fuincional

--- Js/controllers.js
+++ Js/controllers.js
@@ -1,3 +1,5 @@
+//CONTROLADOR DE BANNER SLIDER
+
 const slider = document.querySelector('#slider-contenedor');
 
 const derecha = document.querySelector('#derecha');
@@ -18,3 +20,59 @@ derecha.addEventListener('click', function () {
 izquierda.addEventListener('click', function () {
     back();
 });
+
+//<------------------------------------------------->
+
+//CONTROLADOR DE SLIDER DE SERVICIOS
+
+const slider_servicios = document.querySelector('.slider-servicios');
+
+let slider_secciones = document.querySelectorAll('.item-s');
+
+let sliderLast = slider_secciones[slider_secciones.length - 1];
+
+const boton_izquierdo = document.querySelector('#boton_izquierdo');
+
+const boton_derecho = document.querySelector('#boton_derecho');
+
+slider_servicios.insertAdjacentElement('afterbegin', sliderLast);
+
+function mover_derecha() {
+    let sliderFirst = document.querySelectorAll('.item-s')[0];
+
+    slider_servicios.style.marginLeft = '-200%';
+
+    slider_servicios.style.transition = 'all 0.8s';
+
+    setTimeout(function () {
+        slider_servicios.style.transition = 'none';
+        slider_servicios.insertAdjacentElement('beforeend', sliderFirst);
+        slider_servicios.style.marginLeft = '-100%';
+    }, 800);
+}
+
+function mover_izquierda() {
+    let slider_secciones = document.querySelectorAll('.item-s');
+
+    let sliderLast = slider_secciones[slider_secciones.length - 1];
+
+    slider_servicios.style.marginLeft = '0%';
+
+    slider_servicios.style.transition = 'all 0.8s';
+
+    setTimeout(function () {
+        slider_servicios.style.transition = 'none';
+        slider_servicios.insertAdjacentElement('afterbegin', sliderLast);
+        slider_servicios.style.marginLeft = '-100%';
+    }, 800);
+}
+
+boton_derecho.addEventListener('click', function () {
+    mover_derecha();
+});
+
+boton_izquierdo.addEventListener('click', function () {
+    mover_izquierda();
+});
+
+//<------------------------------------------------->
