82714be2e353d8b502eaeefe199566bb4ea99610
DarkKevo <kevinaraujogonzalez@gmail.com>
2022-03-09 22:01:26 -0400

    Dudas Desplegables Listas

--- Js/controllers.js
+++ Js/controllers.js
@@ -76,3 +76,45 @@ boton_izquierdo.addEventListener('click', function () {
 });
 
 //<------------------------------------------------->
+
+//CONTROLADOR DE MENU DESPLEGABLE
+
+$(document).ready(function () {
+	//Drop Automatico
+	$('.barra1').click(function () {
+		$('#li1').slideToggle();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+	});
+	$('.barra2').click(function () {
+		$('#li2').slideToggle();
+		$('#li1').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+	});
+	$('.barra3').click(function () {
+		$('#li3').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+	});
+
+	$('.barra4').click(function () {
+		$('#li4').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li5').slideUp();
+	});
+	$('.barra5').click(function () {
+		$('#li5').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+	});
+});
