09d6dbf9a631c8dd0689d0f64c4028f8f49e243d
DarkKevo <kevinaraujogonzalez@gmail.com>
2022-03-10 09:51:35 -0400

    Listo los Scrolls

--- Js/controllers.js
+++ Js/controllers.js
@@ -77,74 +77,83 @@ boton_izquierdo.addEventListener('click', function () {
 
 //<------------------------------------------------->
 
-//CONTROLADOR DE MENU DESPLEGABLE
+//CONTROLADOR DE MENU DESPLEGABLE y SCROLLS
 
 $(document).ready(function () {
-	//Drop Automatico
-	$('.barra1').click(function () {
-        $('#icon-1').toggleClass('reverse')
-        $('#icon-2').removeClass('reverse')
-        $('#icon-3').removeClass('reverse')
-        $('#icon-4').removeClass('reverse')
-        $('#icon-5').removeClass('reverse')
-		$('#li1').slideToggle();
-		$('#li2').slideUp();
-		$('#li3').slideUp();
-		$('#li4').slideUp();
-		$('#li5').slideUp();
-	});
-	$('.barra2').click(function () {
-        $('#icon-2').toggleClass('reverse')
-        $('#icon-1').removeClass('reverse')
-        $('#icon-3').removeClass('reverse')
-        $('#icon-4').removeClass('reverse')
-        $('#icon-5').removeClass('reverse')
-		$('#li2').slideToggle();
-		$('#li1').slideUp();
-		$('#li3').slideUp();
-		$('#li4').slideUp();
-		$('#li5').slideUp();
-	});
-	$('.barra3').click(function () {
-        $('#icon-3').toggleClass('reverse')
-        $('#icon-1').removeClass('reverse')
-        $('#icon-2').removeClass('reverse')
-        $('#icon-4').removeClass('reverse')
-        $('#icon-5').removeClass('reverse')
-		$('#li3').slideToggle();
-		$('#li1').slideUp();
-		$('#li2').slideUp();
-		$('#li4').slideUp();
-		$('#li5').slideUp();
-	});
+    //Drop Automatico
+    $('.barra1').click(function () {
+        $('#icon-1').toggleClass('reverse');
+        $('#icon-2').removeClass('reverse');
+        $('#icon-3').removeClass('reverse');
+        $('#icon-4').removeClass('reverse');
+        $('#icon-5').removeClass('reverse');
+        $('#li1').slideToggle();
+        $('#li2').slideUp();
+        $('#li3').slideUp();
+        $('#li4').slideUp();
+        $('#li5').slideUp();
+    });
+    $('.barra2').click(function () {
+        $('#icon-2').toggleClass('reverse');
+        $('#icon-1').removeClass('reverse');
+        $('#icon-3').removeClass('reverse');
+        $('#icon-4').removeClass('reverse');
+        $('#icon-5').removeClass('reverse');
+        $('#li2').slideToggle();
+        $('#li1').slideUp();
+        $('#li3').slideUp();
+        $('#li4').slideUp();
+        $('#li5').slideUp();
+    });
+    $('.barra3').click(function () {
+        $('#icon-3').toggleClass('reverse');
+        $('#icon-1').removeClass('reverse');
+        $('#icon-2').removeClass('reverse');
+        $('#icon-4').removeClass('reverse');
+        $('#icon-5').removeClass('reverse');
+        $('#li3').slideToggle();
+        $('#li1').slideUp();
+        $('#li2').slideUp();
+        $('#li4').slideUp();
+        $('#li5').slideUp();
+    });
 
-	$('.barra4').click(function () {
-        $('#icon-4').toggleClass('reverse')
-        $('#icon-1').removeClass('reverse')
-        $('#icon-2').removeClass('reverse')
-        $('#icon-3').removeClass('reverse')
-        $('#icon-5').removeClass('reverse')
-		$('#li4').slideToggle();
-		$('#li1').slideUp();
-		$('#li2').slideUp();
-		$('#li3').slideUp();
-		$('#li5').slideUp();
-	});
-	$('.barra5').click(function () {
-        $('#icon-5').toggleClass('reverse')
-        $('#icon-1').removeClass('reverse')
-        $('#icon-2').removeClass('reverse')
-        $('#icon-3').removeClass('reverse')
-        $('#icon-4').removeClass('reverse')
-		$('#li5').slideToggle();
-		$('#li1').slideUp();
-		$('#li2').slideUp();
-		$('#li3').slideUp();
-		$('#li4').slideUp();
-	});
+    $('.barra4').click(function () {
+        $('#icon-4').toggleClass('reverse');
+        $('#icon-1').removeClass('reverse');
+        $('#icon-2').removeClass('reverse');
+        $('#icon-3').removeClass('reverse');
+        $('#icon-5').removeClass('reverse');
+        $('#li4').slideToggle();
+        $('#li1').slideUp();
+        $('#li2').slideUp();
+        $('#li3').slideUp();
+        $('#li5').slideUp();
+    });
+    $('.barra5').click(function () {
+        $('#icon-5').toggleClass('reverse');
+        $('#icon-1').removeClass('reverse');
+        $('#icon-2').removeClass('reverse');
+        $('#icon-3').removeClass('reverse');
+        $('#icon-4').removeClass('reverse');
+        $('#li5').slideToggle();
+        $('#li1').slideUp();
+        $('#li2').slideUp();
+        $('#li3').slideUp();
+        $('#li4').slideUp();
+    });
 });
 
 //<---------------------------------------------------->
 
+//CONTROLADOR DE LOS SCROLLS
 
+document.addEventListener('click', function (event) {
+    if (!event.target.matches('.btn-scroll-view')) return;
 
+    event.preventDefault();
+
+    const element = document.getElementById(event.target.dataset.target);
+
+    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
